ab7c790741b7159cee5798022e5a01169ba27f4c
Daniel Lidstrom <dlidstrom@gmail.com>
2010-08-29 18:23:27 +0200

    Moved logo into application helper. Added specifications for links.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,5 +27,34 @@ describe "LayoutLinks" do
     page.should have_css('title', :text => "Sign up")
   end
 
+  it "should have link to About" do
+    visit root_path
+    click_link "About"
+    page.should have_css('title', :text => "About")
+  end
+  
+  it "should have link to Help" do
+    visit root_path
+    click_link "Help"
+    page.should have_css('title', :text => "Help")
+  end
+  
+  it "should have link to Contact" do
+    visit root_path
+    click_link "Contact"
+    page.should have_css('title', :text => "Contact")
+  end
+  
+  it "should have link to Home" do
+    visit root_path
+    click_link "Home"
+    page.should have_css('title', :text => "Home")
+  end
+  
+  it "should have link to Sign up" do
+    visit root_path
+    click_link "Sign up now!"
+    page.should have_css('title', :text => "Sign up")
+  end
+  
 end
-

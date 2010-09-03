1ab56a5f65c2d259a1308157b7474d57375871e1
Daniel Lidstrom <dlidstrom@gmail.com>
2010-09-03 19:21:26 +0200

    User model with passwords, and user show page.

--- Gemfile
+++ Gemfile
@@ -1,16 +1,14 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.0'
-
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
 gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'gravatar_image_tag', '0.1.0'
 
 group :development, :test do
   gem 'rspec-rails', '>= 2.0.0.beta.20'
   gem 'capybara'
   gem 'annotate-models', '1.0.4'
+  gem 'factory_girl_rails', '1.0'
 end
 
 #group :test do
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,5 +14,40 @@ describe UsersController do
       page.should have_css("title", :text => "Sign up")
     end
   end
+  
+  describe "GET 'show'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    it "should be successful" do
+      visit user_path(@user)
+      #get :show, :id => @user
+      page.status_code.should == 200
+    end
+    
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+    
+    it "should have the right title" do
+      visit user_path(@user)
+      #visit :show, :id => @user
+      page.should have_css("title", :text => @user.name)
+    end
+    
+    it "should include the user's name" do
+      visit user_path(@user)
+      #get :show, :id => @user
+      page.should have_css("h1", :text => @user.name)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id => @user
+      page.should have_css("h1>img", :class => "gravatar")
+    end
+  end
 
 end

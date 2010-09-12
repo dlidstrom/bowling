1b03a3a23d6f1865f3d1bba1f0e1b58d59892667
Daniel Lidstrom <dlidstrom@gmail.com>
2010-09-12 23:30:32 +0200

    Done with sign in.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -43,7 +43,6 @@ describe UsersController do
     end
     
     describe "success" do
-      
       before(:each) do
         @attr = { :name => "New User", :email => "user@example.com",
         :password => "foobar", :password_confirmation => "foobar" }
@@ -65,8 +64,11 @@ describe UsersController do
         flash[:success].should =~ /welcome to the sample app/i
       end
       
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     end
-    
   end
   
   describe "GET 'show'" do
